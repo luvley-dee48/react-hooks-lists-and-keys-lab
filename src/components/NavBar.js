00f654db815c9024a154e7b4d753e7f20eae5a6d
deborah <fabbydebby@gmail.com>
2024-05-13 16:35:23 +0300

    list-and-keys-lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {/* display an <a> tag for each link here */}
+      <ul>
+        {/* using map to create a new array of JSX elements */}
+        {links.map((link, index) => (
+          <div key={index} className="nav-link">
+            <a href={`#${link}`}>{link} </a>
+          </div>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
